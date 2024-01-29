9e0fecce4b4ab00a4360233e77a024862635d347
Nhat Thai <nhat.thaiquang@asnet.com.vn>
2024-01-29 10:17:23 +0700

    Add API

---
+++ WebAPI/Program.cs
@@ -0,0 +1,97 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+var configuration = builder.Configuration;
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "MyProject", Version = "v1.0.0" });
+
+    var securitySchema = new OpenApiSecurityScheme
+    {
+        Description = "Using the Authorization header with the Bearer scheme.",
+        Name = "Authorization",
+        In = ParameterLocation.Header,
+        Type = SecuritySchemeType.Http,
+        Scheme = "bearer",
+        Reference = new OpenApiReference
+        {
+            Type = ReferenceType.SecurityScheme,
+            Id = "Bearer"
+        }
+    };
+
+    c.AddSecurityDefinition("Bearer", securitySchema);
+
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        { securitySchema, new[] { "Bearer" } }
+    });
+    
+});
+
+
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+})
+.AddJwtBearer(options =>
+{
+    var auth0Audience = configuration["Auth0:Audience"];
+
+    if (string.IsNullOrEmpty(auth0Audience))
+    {
+        throw new("auth0Audience is empty");
+    }
+
+    options.Authority = $"https://{configuration["Auth0:Domain"]}/";
+
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateAudience = true,
+        ValidAudiences = auth0Audience.Split(";"),
+        ValidateIssuer = true,
+        ValidIssuer = auth0Audience
+    };
+});
+
+builder.Services.AddCors(c =>
+{
+    var originWeb = configuration.GetValue<string>("OriginWeb");
+
+    if (!string.IsNullOrEmpty(originWeb))
+    {
+        c.AddPolicy("AllowOrigin", builder => builder.WithOrigins(originWeb).AllowAnyMethod().AllowAnyHeader().AllowCredentials());
+    }
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseCors("AllowOrigin");
+
+app.UseHttpsRedirection();
+
+app.UseAuthentication();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
